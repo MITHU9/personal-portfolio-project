0c33a6acac79758c302dc8707b23c184a51e1098
Shahariar Mithu <kmmithu2015@gmail.com>
2024-06-24 01:37:14 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+/* ===================Typing Animation=============== */
+var typed = new Typed(".typing", {
+  strings: [
+    "Frontend Developer!",
+    "Cyber Security Learner!",
+    "Quick Learner!",
+    "CS Student!",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
